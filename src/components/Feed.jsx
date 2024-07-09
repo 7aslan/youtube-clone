3febe95c7fe78943c409795a9ac841a5bc38ecd8
Samet Aslan <sametaslan1710@gmail.com>
2024-07-09 07:00:23 +0300

    add: Videos.jsx - fix: apiKey usage method on fetchFromAPI.jsx due to working problem

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { Sidebar, Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
-
+  const [videos, setVideos] = useState([]);
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
   }, []);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -39,7 +41,7 @@ const Feed = () => {
         >
           {selectedCategory} <span style={{ color: "#f31503" }}>videos</span>
         </Typography>
-        <Videos videos={[]} />
+        <Videos videos={[videos]} />
       </Box>
     </Stack>
   );
--- src/utils/fetchFromAPI.jsx
+++ src/utils/fetchFromAPI.jsx
@@ -4,14 +4,19 @@ const options = {
   method: "GET",
   params: {
     maxResults: "50",
+    order: "date",
   },
   headers: {
-    "x-rapidapi-key": import.meta.env.REACT_APP_RAPID_API_KEY,
+    "x-rapidapi-key": "8996fc2622msha1200be8c3af7d4p143ba4jsnf2a2ff47612f",
     "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
   },
 };
-
 export const fetchFromAPI = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-  return data;
+  try {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+  } catch (error) {
+    console.error("Error fetching data from API:", error);
+    throw error;
+  }
 };
