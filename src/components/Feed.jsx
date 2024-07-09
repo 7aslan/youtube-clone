97283c40556e1dcb420651bcc72e83120093775e
Samet Aslan <sametaslan1710@gmail.com>
2024-07-09 06:31:20 +0300

    add: functionality of selecting category

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -18,7 +18,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -34,7 +37,7 @@ const Feed = () => {
           mb={2}
           sx={{ color: "white" }}
         >
-          New <span style={{ color: "#f31503" }}>videos</span>
+          {selectedCategory} <span style={{ color: "#f31503" }}>videos</span>
         </Typography>
         <Videos videos={[]} />
       </Box>
