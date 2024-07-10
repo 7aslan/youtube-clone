b3373efa15e139bd0a509a538f19c6294649d30e
Samet Aslan <sametaslan1710@gmail.com>
2024-07-11 01:30:30 +0300

    add: channelDetail.jsx and a new page for this component

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,7 +3,7 @@ import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
   return (
     <Box
       sx={{
@@ -15,6 +15,7 @@ const ChannelCard = ({ channelDetail }) => {
         width: { xs: "356px", md: "320px" },
         height: "326px",
         margin: "auto",
+        marginTop: marginTop,
       }}
     >
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
@@ -28,7 +29,6 @@ const ChannelCard = ({ channelDetail }) => {
           }}
         >
           <CardMedia
-          
             image={
               channelDetail?.snippet?.thumbnails?.high?.url ||
               demoProfilePicture
