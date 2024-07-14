df15d108b7b9b7ee2ea28877e9c43aecb99800b5
Samet Aslan <sametaslan1710@gmail.com>
2024-07-14 14:48:33 +0300

    add: videoDetail.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,16 @@
 import { VideoCard, ChannelCard } from "./";
 import { Stack, Box } from "@mui/material";
-const Videos = ({ videos }) => {
-  console.log(videos,"videoId")
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return "Loading...";
+
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item,index) => (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
+      {videos.map((item, index) => (
         <Box key={index}>
           {item?.id?.videoId && <VideoCard video={item} />}
           {item?.snippet?.videoID && <ChannelCard channelDetail={item} />}
