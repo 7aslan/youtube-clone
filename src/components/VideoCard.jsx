983f6ccb6cd9f4538c7edc89ca4274ababbab152
Samet Aslan <sametaslan1710@gmail.com>
2024-07-14 14:51:41 +0300

    fix: VideoCard.jsx for responsive design

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -15,11 +15,10 @@ const VideoCard = ({
     snippet,
   },
 }) => {
-
   return (
     <Card
       sx={{
-        width: { md: "320px", xs: "100px" },
+        width: { xs: "100%", sm: "358px", md: "320px" },
         boxShadow: "none",
         borderRadius: 0,
       }}
@@ -27,7 +26,13 @@ const VideoCard = ({
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{
+            width: {
+              xs: "100%",
+              sm: "358px",
+            },
+            height: 180,
+          }}
           image={snippet?.thumbnails?.high?.url}
         />
       </Link>
